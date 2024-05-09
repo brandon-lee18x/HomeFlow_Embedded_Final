6400681bf4a216623a058dff5aefd907a7ddddf5
brandon-lee18x <brandon_lee@umail.ucsb.edu>
2024-05-09 14:59:16 -0700

    got temp sensor working

--- src/main.c
+++ src/main.c
@@ -15,6 +15,11 @@
 #include "../inc/hr.h"
 #include "../inc/imu.h"
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/devicetree.h>
+
+const struct device* i2c0_dev = DEVICE_DT_GET(DT_NODELABEL(i2c0));
 
 int main(void)
 {
@@ -68,13 +73,18 @@ int main(void)
 		return 0;
 	}
 
-	for (;;) {
+
+	while (1) {
 		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
 		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
 
-		char c_string[20] = "BMT:25.5";
+		//temp sensor
+		float temp = get_body_temp();
+		char temp_buf[10];
+		snprintf(temp_buf, sizeof(temp_buf), "BTS:%f", temp);
+
 		if (current_conn) {
-			bt_nus_send(current_conn, c_string, strlen(c_string));
+			bt_nus_send(current_conn, temp_buf, strlen(temp_buf));
 		}
 	}
 	
